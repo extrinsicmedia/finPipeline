d83cef2b4168209f337985c9a5b8d6f0204c1ea7
extrinsicmedia <miles@finvfx.com>
2013-07-09 08:41:20 -0700

    Starting work on ffmpegCmds.py

--- ffmpeg/examples.sh
+++ ffmpeg/examples.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# mp4 1 pass encoding from http://transcoding.wordpress.com/2012/06/17/optimal-h-264-encoding-for-flash-and-html5/
+ffmpeg -i “input.mov” -vf scale=768:432 -vcodec libx264 -preset slow -tune film -y -an -crf 22 temp.m4v
+
 # mp4 2 pass encoding from http://transcoding.wordpress.com/2012/06/17/optimal-h-264-encoding-for-flash-and-html5/
 ffmpeg -i some_file_in.mp4 -vf scale=768:432 -pass 1 -sws_flags lanczos -vcodec libx264 -preset slow -tune film -y -an -b:v 1000k -bufsize 2000k -f rawvideo NUL && ffmpeg -i some_file_in.mp4 -vf scale=768:432 -pass 2 -sws_flags lanczos -vcodec libx264 -preset slow -tune film -y -an -b:v 1000k -bufsize 2000k -f mp4 some_file_out.m4v
-
--- python/fin/fin/fileOps/duOps.py
+++ python/fin/fin/fileOps/duOps.py
@@ -20,8 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/gpl.html/>.
 from subprocess import Popen, PIPE, STDOUT
 from os.path import isdir, isfile
 
-#path = sys.argv[1]
-
 # modules
 def duDirAll(dirPath):
     if isdir(dirPath):
