36782c321250dd3cdacfe7fcd05f14626fbdf7f6
extrinsicmedia <miles@finvfx.com>
2013-10-18 10:27:05 -0700

    Lion 10.7.5 install settings started

--- ffmpeg/test.sh
+++ ffmpeg/test.sh
@@ -7,13 +7,31 @@ then
         if [ $2 ];
         then
             # mp4 1 pass encoding from http://transcoding.wordpress.com/2012/06/17/optimal-h-264-encoding-for-flash-and-html5/
-            #ffmpeg -i $1 -vf scale=768:432 -vcodec libx264 -preset slow -tune film -y -an -crf 22 test01.m4v
+            ffmpeg -i $1 -vcodec libx264 -preset slow -tune film -q:v 9 -y -an -crf 22 $2_1pass.m4v
 
             # mp4 2 pass encoding from http://transcoding.wordpress.com/2012/06/17/optimal-h-264-encoding-for-flash-and-html5/
-            ffmpeg -i $1 -vf scale=768:432 -pass 1 -sws_flags lanczos -vcodec libx264 -preset slow -tune film -y -an -b:v 1000k -bufsize 2000k -f rawvideo /dev/null && ffmpeg -i $1 -vf scale=768:432 -pass 2 -sws_flags lanczos -vcodec libx264 -preset slow -tune film -y -an -b:v 1000k -bufsize 2000k -f mp4 $2
-        else
-            echo "Please enter an output name with *.m4v"
+            ffmpeg -i $1 -pass 1 -sws_flags lanczos -vcodec libx264 -preset slow -tune film -q:v 9 -y -an -b:v 1000k -bufsize 2000k -f rawvideo /dev/null && ffmpeg -i $1 -pass 2 -sws_flags lanczos -vcodec libx264 -preset slow -tune film -q:v 9 -y -an -b:v 1000k -bufsize 2000k -f mp4 $2_2pass.m4v
+
+            # prores 0, 4:2:2
+            ffmpeg -i $1 -vcodec prores_kostya -profile:v 0 -q:v 9 -y -an -crf 22 $2_prores0.mov
+        
+            # prores 1
+            ffmpeg -i $1 -vcodec prores_kostya -profile:v 1 -q:v 9 -y -an -crf 22 $2_prores1.mov
+
+            # prores 2
+            ffmpeg -i $1 -vcodec prores_kostya -profile:v 2 -q:v 9 -y -an -crf 22 $2_prores2.mov
+
+            # prores 3
+            ffmpeg -i $1 -vcodec prores_kostya -profile:v 3 -q:v 9 -y -an -crf 22 $2_prores3.mov
+
+            # Motion jpeg
+            ffmpeg -i $1 -vf scale=1920:1080 -vcodec mjpeg -qscale 1 -tune film -q:v 9 -y -an -crf 22 $2_mjpeg.mov
+
+            # DNXHD 36M
+            ffmpeg -i $1 -vf scale=1920:1080 -vcodec dnxhd -b 36M -tune film -q:v 9 -y -an -crf 22 $2_dnxhd36M.mov
+
+            # DNXHD 115M
+            ffmpeg -i $1 -vf scale=1920:1080 -vcodec dnxhd -b 115M -tune film -q:v 9 -y -an -crf 22 $2_dnxhd115M.mov
+        fi
     fi
-    else
-        echo "Please enter a movie file path"
 fi
---
+++ os/debian-10.7.5/debian-10.7.5_installs_2013-10-17.sh
@@ -0,0 +1,68 @@
+# New way to install all ffmpeg options
+brew install -v ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-frei0r --with-libass --with-libaca --with-libvo-aacenc --with-libvorbis --with-libvpx --with-openjpeg --with-openssl --with-schroedinger --with-speex --with-theora --with-tools 
+
+# New way to install imagemagick with all options
+brew install -v imagemagick --with-fontconfig --with-jasper --with-liblqr --with-librsvg --with-libtiff --with-libwmf --with-little-cms --with-openexr --with-quantum-depth-32 --with-webp --with-x11
+
+### Start FFMBC install
+# Helpful instructions here: http://www.movieeditor.com/2012/01/26/building-ffmbcffmpeg-on-mac-os-x-lion/
+# FFMBC dependencies - Some of these may be covered by new ffmpeg install
+brew install -v frei0r libdc1394 dirac
+
+wget https://ffmbc.googlecode.com/files/FFmbc-0.7-rc8.tar.bz2
+tar -xvjf FFmbc-0.7-rc8.tar.bz2
+cd FFmbc-0.7-rc8/
+./configure --enable-gpl --enable-nonfree --enable-shared --enable-postproc --enable-runtime-cpudetect --enable-frei0r --enable-libdc1394 --enable-libdirac --enable-libfaac --enable-libmp3lame --enable-libopenjpeg --enable-libschroedinger --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-pthreads --enable-libxvid --enable-zlib
+
+
+# Install qt, python, python3, pyqt, boost, cmake, hdf5, ilmbase, openexr, alembic, htop
+brew install -v cmake
+brew install -v gnupg --8192
+brew install -v pass
+brew install -v keychain
+brew install -v openssl
+brew install -v python --universal --with-brewed-openssl
+brew install -v ilmbase
+brew install -v openexr
+brew install -v git
+
+brew install -v qt --universal --with-docs --with-mysql
+brew install -v python3
+brew install -v pyqt
+brew install -v boost
+brew install -v hdf5
+brew install -v alembic
+brew install -v htop
+
+
+
+# Update python3 setuptools and pip
+sudo pip3 install --upgrade setuptools
+sudo pip3 install --upgrade pip
+
+## Manual installs
+# Komodo Edit
+# VLC
+# Handbrake
+# True Crypt
+# Backblaze
+# XCode
+# XCode Command Line
+# Firefox
+# Celtx
+# Meshlab
+# Adobe CC
+# Maya
+# Nuke
+# 3Delight
+# Celtx
+# djv
+# GoProStudio
+# Meshlab
+# Wacom Utility
+# Blender
+# Arduino
+# OpenOffice
+# Virtualbox
+# Cuda
+
