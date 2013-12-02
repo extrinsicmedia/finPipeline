5cf07b15ada65f7afb99d0d4427c7abf775054e1
extrinsicmedia <miles@finvfx.com>
2013-12-02 13:17:38 -0800

    Update

--- nuke/init.py
+++ nuke/init.py
@@ -1,17 +1,29 @@
 ''' init.py for use with finPipeline
 Copyright (C) 2012  Miles Lauridsen
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License.
+Based on BSD 2-Clause License  http://opensource.org/licenses/BSD-2-Clause
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/gpl.html/>.'''
+Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+'''
 
 import os
 import platform
@@ -74,8 +86,9 @@ if nuke.env['MACOS']:
 
 ## get all folders in $SHARED_SERVER location and add their path to Nuke
 nukeDirs = []
-for dirname, dirnames, filenames in os.walk(os.environ.get('NUKE_PATH', None) ):
+for dirname, dirnames, filenames in os.walk(os.environ.get('NUKE_STARTUP', None) ):
     for subdirname in dirnames:
+        print "Appending:", os.path.join(dirname, subdirname)
         nukeDirs.append(os.path.join(dirname, subdirname))
 
 for path in nukeDirs:
@@ -83,7 +96,7 @@ for path in nukeDirs:
 
     
 ## LUMA gizmo collector
-CUSTOM_GIZMO_LOCATION = os.path.join(os.environ.get('NUKE_PATH', None), 'gizmos')
+CUSTOM_GIZMO_LOCATION = os.path.join(os.environ.get('NUKE_STARTUP', None), 'gizmos')
 
 class GizmoPathManager(object):
     def __init__(self, exclude=r'^\.', searchPaths=None):
