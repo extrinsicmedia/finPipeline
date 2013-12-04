9a4fcd41945e95e5a5f2cdf09d57ec7ea4ddfea9
extrinsicmedia <miles@finvfx.com>
2013-12-03 19:57:54 -0800

    Changing to BSD license and custom read/write have non overriding hotkeys

--- nuke/menu.py
+++ nuke/menu.py
@@ -1,17 +1,29 @@
 ''' menu.py for use with finPipeline
 Copyright (C) 2012  Miles Lauridsen
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License.
+Based on BSD 2-Clause License  http://opensource.org/licenses/BSD-2-Clause
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/gpl.html/>.'''
+Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+'''
 
 import os
 import re
@@ -64,7 +76,7 @@ def createPaths():
                 create = True
     if create == True:
         nuke.message( "Paths have been created" )
-        
+
 ### END DEFINITIONS ###
 
 
@@ -102,7 +114,6 @@ nuke.knobDefault( 'EXPTool.mode', 'Stops' )
 
 # Node Colors
 nuke.knobDefault( 'Transform.tile_color', '1278560767.0' )
-
 ### END DEFAULTS SETUP ###
 
 ### BEGIN LUMA GIZMO SETUP ###
@@ -128,8 +139,8 @@ def customRead():
         n['DB'].setFlag( 0 )
 
 # ADD CUSTOM READ AND WRITE TO TOOLBAR
-nuke.menu( 'Nodes' ).addCommand( 'Image/WriteAsset', lambda: nuke.createNode( 'WriteAsset' ), 'w' )
-nuke.menu( 'Nodes' ).addCommand( 'Image/Read', customRead, 'r' )
+nuke.menu( 'Nodes' ).addCommand( 'Image/WriteAsset', lambda: nuke.createNode( 'WriteAsset' ), 'shift-w' )
+nuke.menu( 'Nodes' ).addCommand( 'Image/Read', customRead, 'shift-r' )
 
 # ADD EASY SAVE TO SHOT MENU
 shotMenu = '%s - %s' % ( os.getenv( 'SEQ' ), os.getenv('SHOT') )
