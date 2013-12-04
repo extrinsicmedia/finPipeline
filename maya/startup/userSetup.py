77b77025b30633b88619d7a916d00d19c11ab33c
extrinsicmedia <miles@finvfx.com>
2013-12-03 19:21:01 -0800

    Changed cmds.evalDeferred so that plugins load after setting plugin path

--- maya/startup/userSetup.py
+++ maya/startup/userSetup.py
@@ -4,17 +4,28 @@
 userSetup.py - startup file for Maya users
 Copyright (C) 2012  Miles Lauridsen
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License.
+Based on BSD 2-Clause License  http://opensource.org/licenses/BSD-2-Clause
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/gpl.html/>.
+Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
 '''
 
 ### Symlink this file to the user script directory
@@ -40,10 +51,12 @@ import finUserSetup as fus
 def printConfirm():
     print "FIN Startup Complete"
 
-pm.mayautils.executeDeferred( 'fus.osPlugins()' )
-pm.mayautils.executeDeferred( 'fus.loadPlugins()' )
-pm.mayautils.executeDeferred( 'fus.initialStructure()' )
-pm.mayautils.executeDeferred( 'fus.newStuff()' )
-pm.mayautils.executeDeferred( 'fus.startupCheck()' )
-pm.mayautils.executeDeferred( 'printConfirm()' )
+#pm.mayautils.executeDeferred( 'fus.loadPlugins()' ) # this was the old way of loading
+cmds.evalDeferred( 'fus.osPlugins()' )
+cmds.evalDeferred('lowestPriority')
+cmds.evalDeferred('fus.loadPlugins()')
+cmds.evalDeferred( 'fus.initialStructure()' )
+cmds.evalDeferred( 'fus.newStuff()' )
+cmds.evalDeferred( 'fus.startupCheck()' )
+cmds.evalDeferred( 'printConfirm()' )
 
