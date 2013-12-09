925d2e45c4a5939b0936540aed4c764760102f46
extrinsicmedia <miles@finvfx.com>
2013-12-08 21:55:28 -0800

    Fully prepped for Linux application paths

--- bash/unix_config_global.sh
+++ bash/unix_config_global.sh
@@ -74,19 +74,44 @@ alias rsynctimes='rsync $RSYNC_TIMES --log-file=$SYSTEMS_SERVER/database/logs/rs
 PATH="$SYSTEMS_SERVER/bin/frametools:$MAYA_LOCATION/bin:$PATH"
 alias makedate='mkdir $(date +%Y-%m-%d)'
 alias makedatetime='mkdir $(date +%Y-%m-%d_%H%M%S)'
-#export BOOST_ROOT="/usr/local/Cellar/boost/1.49.0"
 
 ## Python - relying on a properly set PYTHONPATH in user's .bashrc
 ## Additonal libraries could be placed here, but THINK critically
 ## if that is the best idea
 #export PYTHONPATH="$PYTHONPATH"
 
+## Nuke
+export NUKE_PATH="$SYSTEMS_SERVER/nuke"
+export NUKE_STARTUP="$SYSTEMS_SERVER/nuke"
+
+## Maya
+export MAYA_SHARED_DIR="$SYSTEMS_SERVER/maya"
+export MAYA_FIN_SCRIPT_PATH="$MAYA_SHARED_DIR/mel"
+export MAYA_EXTERNAL_SCRIPT_PATH="$MAYA_SHARED_DIR/external/mel"
+export MAYA_SCRIPT_PATH="$MAYA_FIN_SCRIPT_PATH:MAYA_EXTERNAL_SCRIPT_PATH"
+export MAYA_FIN_PLUGIN_PATH="$MAYA_SHARED_DIR/plugins"
+export MAYA_EXTERNAL_PLUGIN_PATH="$MAYA_SHARED_DIR/external/plugins"
+export MAYA_PLUG_IN_PATH="$MAYA_FIN_PLUGIN_PATH:$MAYA_EXTERNAL_PLUGIN_PATH"
+export MAYA_FIN_ICON_PATH="$MAYA_SHARED_DIR/icons"
+export MAYA_EXTERNAL_ICON_PATH="$MAYA_SHARED_DIR/external/icons"
+export XBMLANGPATH="$MAYA_FIN_ICON_PATH:$MAYA_EXTERNAL_ICON_PATH"
+export MAYA_FIN_SHELF_PATH="$MAYA_SHARED_DIR/shelves"
+export MAYA_EXTERNAL_SHELF_PATH="$MAYA_SHARED_DIR/external/shelves"
+export MAYA_SHELF_PATH="$MAYA_FIN_SHELF_PATH:$MAYA_EXTERNAL_SHELF_PATH"
+export MAYA_FIN_PYTHON_PATH="$MAYA_SHARED_DIR/python"
+export MAYA_EXTERNAL_PYTHON_PATH="$MAYA_SHARED_DIR/external/python"
+export MAYA_PYTHON_PATH="$MAYA_FIN_PYTHON_PATH:$MAYA_EXTERNAL_PYTHON_PATH"
+
+# Dynamic shelf variables here so Maya.env doesn't need to be set
+export MAYA_DYN_SHELF_NAME="finShelf"
+
 ## OCIO Default setup
 export OCIO="$SYSTEMS_SERVER/ocio/spi-vfx/config.ocio"
 
-### Check OS and set paths accordingly based on that info ###
+### OSX Application paths ###
 if [ `uname` == "Darwin" ]; then
     ### Apps
+    #export BOOST_ROOT="/usr/local/Cellar/boost/1.49.0"
     alias truecrypt='/Applications/TrueCrypt.app/Contents/MacOS/TrueCrypt'
     alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
 
@@ -107,8 +132,6 @@ if [ `uname` == "Darwin" ]; then
     alias djview-0.9.0='/Applications/djv-0.9.0.app/Contents/MacOS/djv-0.9.0'
     alias djview-0.8.3='/Applications/djv-0.8.3-pre2.app/Contents/MacOS/djv-0.8.3-pre2'
     export DJV_PATH='/Applications/djv-0.9.0.app/Contents/MacOS/djv-0.9.0'
-    
-# Need to add Linux directory to djview
 
     ## RV
     alias rv='/Applications/RV64.app/Contents/MacOS/RV64'
@@ -135,7 +158,6 @@ if [ `uname` == "Darwin" ]; then
     alias maya='/Applications/Autodesk/maya2014/Maya.app/Contents/bin/maya'
     alias maya2014='/Applications/Autodesk/maya2014/Maya.app/Contents/bin/maya'
     export MAYA_LOCATION="/Applications/Autodesk/maya2014/Maya.app/Contents"
-    export MAYA_SHARED_DIR="$SYSTEMS_SERVER/maya"
     
     # Add Maya bin folder to $PATH
     export PATH="/Applications/Autodesk/maya2014/Maya.app/Contents/bin:${PATH}"
@@ -144,28 +166,59 @@ if [ `uname` == "Darwin" ]; then
     #MAYA_LICENSE=unlimited; export MAYA_LICENSE
     #MAYA_LICENSE_METHOD=Network; export MAYA_LICENSE_METHOD
     #MAYA_ALT_EN=/var/flexlm/maya.lic; export MAYA_ALT_EN
-    
-    export MAYA_FIN_SCRIPT_PATH="$MAYA_SHARED_DIR/mel"
-    export MAYA_EXTERNAL_SCRIPT_PATH="$MAYA_SHARED_DIR/external/mel"
-    export MAYA_SCRIPT_PATH="$MAYA_FIN_SCRIPT_PATH:MAYA_EXTERNAL_SCRIPT_PATH"
-    export MAYA_FIN_PLUGIN_PATH="$MAYA_SHARED_DIR/plugins"
-    export MAYA_EXTERNAL_PLUGIN_PATH="$MAYA_SHARED_DIR/external/plugins"
-    export MAYA_PLUG_IN_PATH="$MAYA_FIN_PLUGIN_PATH:$MAYA_EXTERNAL_PLUGIN_PATH"
-    export MAYA_FIN_ICON_PATH="$MAYA_SHARED_DIR/icons"
-    export MAYA_EXTERNAL_ICON_PATH="$MAYA_SHARED_DIR/external/icons"
-    export XBMLANGPATH="$MAYA_FIN_ICON_PATH:$MAYA_EXTERNAL_ICON_PATH"
-    export MAYA_FIN_SHELF_PATH="$MAYA_SHARED_DIR/shelves"
-    export MAYA_EXTERNAL_SHELF_PATH="$MAYA_SHARED_DIR/external/shelves"
-    export MAYA_SHELF_PATH="$MAYA_FIN_SHELF_PATH:$MAYA_EXTERNAL_SHELF_PATH"
-    export MAYA_FIN_PYTHON_PATH="$MAYA_SHARED_DIR/python"
-    export MAYA_EXTERNAL_PYTHON_PATH="$MAYA_SHARED_DIR/external/python"
-    export MAYA_PYTHON_PATH="$MAYA_FIN_PYTHON_PATH:$MAYA_EXTERNAL_PYTHON_PATH"
-    
-    # Dynamic shelf variables here so Maya.env doesn't need to be set
-    export MAYA_DYN_SHELF_NAME="finShelf"
 
     ## Komodo
     alias komodo='/Applications/Komodo\ Edit\ 8.app/Contents/MacOS/komodo'
+fi
+
+### Linux Application paths - 'Linux' may need to be 'Linux2' ###
+if [ `uname` == "Linux" ]; then
+    
+    ### Application alias and ENV vars ###
+    ## Misc Apps
+    alias truecrypt='echo "set TrueCrypt path"'
+    alias vlc='echo "set VLC path"'
+
+    ## Firefox
+    alias firefox='echo "set Firefox path"'
+
+    ## Setup Amazon EC2 Command-Line Tools
+    export EC2_HOME=~/.ec2
+    export PATH=$PATH:$EC2_HOME/bin
+    export EC2_PRIVATE_KEY=`ls $EC2_HOME/pk-*.pem`
+    export EC2_CERT=`ls $EC2_HOME/cert-*.pem`
+    export JAVA_HOME='echo "set JAVA_HOME path"'
+
+    ## Djview Alias
+    alias djview='echo "set DJView path"'
+    alias djview-0.9.0='echo "set DJView path"'
+    alias djview-0.8.3='echo "set DJView path"'
+    export DJV_PATH='echo "set DJView path"'
+
+    ## RV
+    alias rv='echo "set RV path"'
+    alias rvpkg='echo "set RVPkg path"'
+
+    ## Nuke
+    alias nukex='echo "set NukeX path"'
+    alias nuke='echo "set Nuke path"'
+    alias nuke70v8='echo "set Nuke 7.0v8 path"'
+    
+    ## Maya
+    alias maya='echo "set Maya path"'
+    alias maya2014='echo "set Maya 2014 path"'
+    export MAYA_LOCATION='echo "set Maya Locations path"'
+    
+    # Add Maya bin folder to $PATH
+    export PATH='echo "add Maya bin to $PATH: /usr/<path-to>/bin:${PATH}"'
+
+    # Change this based on facility license settings
+    #MAYA_LICENSE=unlimited; export MAYA_LICENSE
+    #MAYA_LICENSE_METHOD=Network; export MAYA_LICENSE_METHOD
+    #MAYA_ALT_EN=/var/flexlm/maya.lic; export MAYA_ALT_EN
+
+    ## Komodo
+    alias komodo='echo "set Komodo path"'
 fi
 
 ### jobStart tool function
