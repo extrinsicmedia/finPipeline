c431ebf080a8a06fbbb609e66d7600be1f7790eb
extrinsicmedia <miles@finvfx.com>
2014-02-04 08:39:42 -0800

    Adding notes for gpg and ulimit settings

---
+++ bash/notes.sh
@@ -0,0 +1,9 @@
+# http://irtfweb.ifa.hawaii.edu/~lockhart/gpg/
+# list the keys in your public key ring:
+gpg --list-keys
+
+# list the keys in your secret key ring:
+gpg --list-secret-keys
+
+# Change files open limit
+ulimit -n 1024
