01fb4f3fd70c76d102b1980a7c367d0887050795
extrinsicmedia <miles@finvfx.com>
2014-05-21 23:07:36 -0700

    Updating

--- bash/notes.sh
+++ bash/notes.sh
@@ -7,3 +7,12 @@ gpg --list-secret-keys
 
 # Change files open limit
 ulimit -n 1024
+
+# list all connected drives OSX
+diskutil list
+
+# mount drive OSX - replace '/dev/disk1s2' with drive identifier
+diskutil mount /dev/disk1s2
+
+# unmount drive OSX - replace '/dev/disk1s2' with drive identifier
+diskutil unmount /dev/disk1s2
