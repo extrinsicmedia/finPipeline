c6276b38be1261fc858b1ad1c0ddbb6fe9a2d448
extrinsicmedia <miles@finvfx.com>
2015-03-22 21:23:45 -0700

    Updating application stack

--- os/darwin/10.9/darwin-10.9_cask.sh
+++ os/darwin/10.9/darwin-10.9_cask.sh
@@ -3,7 +3,10 @@
 brew install caskroom/cask/brew-cask
 
 brew cask install google-chrome
+brew cask install google-drive
+brew cask install chromecast
 brew cask install firefox
+brew cask install adium
 brew cask install redcine-x-pro
 brew cask install dropbox
 brew cask install transmission
@@ -14,15 +17,20 @@ brew cask install adobe-reader
 brew cask install flash
 brew cask install adobe-air
 brew cask install backblaze
+brew cask install gimp
 brew cask install blender
 brew cask install cyberduck
+brew cask install tunnelblick
 brew cask install djv
 brew cask install handbrake
 brew cask install meshlab
 brew cask install komodo-edit
+brew cask install sequel-pro
+brew cask install robomongo
 brew cask install silverlight
 brew cask install processing
 brew cask install skype
 brew cask install vagrant
 brew cask install virtualbox
 brew cask install vlc
+brew cask install xquartz
