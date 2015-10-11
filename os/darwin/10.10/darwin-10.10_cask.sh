02194c7766a55190dfb067472b1d94a57749ee8b
vector thirteen <vectorthirteen@vectors-MacBook-Air.local>
2015-10-11 10:43:11 -0700

    Adding new 10.10 homebrew installs
    Adding new install lists for 10.10

---
+++ os/darwin/10.10/darwin-10.10_cask.sh
@@ -0,0 +1,30 @@
+### Cask Installs
+
+brew install caskroom/cask/brew-cask
+
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install chromecast
+brew cask install firefox
+brew cask install adium
+brew cask install redcine-x-pro
+brew cask install dropbox
+brew cask install transmission
+brew cask install adobe-creative-cloud
+brew cask install backblaze
+brew cask install gimp
+brew cask install blender
+brew cask install cyberduck
+brew cask install tunnelblick
+brew cask install djv
+brew cask install handbrake
+brew cask install meshlab
+brew cask install komodo-edit
+brew cask install sequel-pro
+brew cask install silverlight
+brew cask install processing
+brew cask install skype
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install vlc
+brew cask install xquartz
---
+++ os/darwin/10.10/darwin-10.10_homebrew.sh
@@ -0,0 +1,83 @@
+### Homebrew installs
+
+# Compilers and Core requirements
+brew install -v cmake
+brew install -v gcc
+brew install -v tbb
+brew install -v glib
+
+# Security
+brew install -v gnupg --8192
+brew install -v pass
+brew install -v keychain
+brew install -v openssl
+
+# Utilities
+brew install -v curl
+brew install -v git
+brew install -v wget
+brew install -v vim
+brew install -v colordiff
+brew install -v coreutils
+brew install -v unrar
+brew install -v htop
+brew install -v nmap
+brew install -v ncdu
+brew install -v youtube-dl
+
+# Database
+brew install -v postgresql
+brew install -v mongodb
+
+# Development
+brew install -v qt --universal --with-docs --with-mysql
+brew install -v scons
+brew install -v boost --with-mpi --with-python --without-single
+brew install -v doxygen
+brew install -v jsonpp
+brew install -v libyaml
+brew install -v fltk
+brew install -v gdk-pixbuf
+brew install -v pixman
+brew install -v harfbuzz
+brew install -v fontconfig
+brew install -v pango
+brew install -v sphinx
+
+# Languages
+brew install -v python --universal --with-brewed-openssl
+brew install -v pyqt
+brew install -v pyside
+
+# Sys-Admin and Security tools
+brew install -v wireshark --with-qt --with-x --with-lua --devel
+brew install -v duplicity
+brew install -v collectd --debug --java
+brew install -v osxfuse
+brew install -v boot2docker
+brew install -v clamav
+
+# Homebrew science
+brew tap homebrew/science
+brew install -v hdf5
+brew install -v alembic
+
+# Imaging
+brew install -v ilmbase
+brew install -v openexr
+brew install -v opencolorio --with-docs --with-python
+brew install -v openimageio # --with-qt and --with-tests currently fail
+
+# FFmpeg
+brew install -v ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-frei0r --with-libass --with-libaca --with-libvo-aacenc --with-libvorbis --with-libvpx --with-openjpeg --with-openssl --with-speex --with-theora --with-tools
+
+# Imagemagick
+brew install -v imagemagick --enable-hdri --with-fftw --with-fontconfig --with-jp2 --with-libtiff --with-little-cms --with-liblqr --with-librsvg --with-little-cms2 --with-openexr --with-webp --with-perl --with-quantum-depth-32 --with-x11
+
+# Image processing
+brew install -v opencv --with-cuda --with-eigen --with-ffmpeg --with-java --with-openexr --with-openni --with-qt --with-tests
+brew install -v pixie
+brew install -v open-scene-graph --docs --with-collada-dom --with-ffmpeg --with-gnuplot --with-openexr
+
+# Link apps such as Python and QT Designer
+sudo brew linkapps
