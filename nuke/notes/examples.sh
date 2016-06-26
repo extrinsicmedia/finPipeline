6cee68535f4869923bd184825d2c56895cc8c502
Extrinsic Media <miles@finvfx.com>
2016-06-25 17:18:36 -0700

    Removing nuke files and directories as they've been moved to extrinsicmedia/finNuke

--- nuke/notes/examples.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Examples for Nuke command line tips should go here
-
-nuke -F 1-100 -ix -V /server_path/some_script.nk
--- nuke/python/guiCheck.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-for n in nuke.allNodes(recurseGroups=True):
-    for knob in n.knobs():
-        if n[knob].hasExpression():
-            origExpression = n[knob].toScript()
-            if "$gui" in origExpression:
-                print n.name()
