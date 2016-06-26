6d7d79e300c2d2903cfd18d4e1d58d6d9d236fe4
Extrinsic Media <miles@finvfx.com>
2016-06-25 17:22:24 -0700

    Removing manual downloads that are available via cask.

--- os/darwin/10.9/darwin-10.9_downloads.sh
+++ os/darwin/10.9/darwin-10.9_downloads.sh
@@ -1,38 +1,7 @@
 ### Downloads
 
-# Komodo - Changing to use Sublime
-#wget http://downloads.activestate.com/Komodo/releases/8.5.3/Komodo-Edit-8.5.3-14067-macosx-x86_64.dmg
-
-# VLC - Download with Boxen or Cask
-#wget http://mirror.csclub.uwaterloo.ca/vlc/vlc/2.1.4/macosx/vlc-2.1.4.dmg
-
-# Handbrake - Download with Boxen or Cask
-#wget http://handbrake.fr/rotation.php?file=HandBrake-0.9.9-MacOSX.6_GUI_x86_64.dmg
-
-# Transmission - Downloaded with Boxen or Cask
-#wget http://download.transmissionbt.com/files/Transmission-2.82.dmg
-
-# BT Sync - Download with Boxen or Cask
-#wget http://download-lb.utorrent.com/endpoint/btsync/os/osx/track/stable/BTSync.dmg
-
-# Virtualbox Extension Pack
-#wget http://download.virtualbox.org/virtualbox/4.3.8/Oracle_VM_VirtualBox_Extension_Pack-4.3.8-92456.vbox-extpack
-
 # Wacom drivers
 wget http://cdn.wacom.com/U/Drivers/Mac/Consumer/533/PenTablet_5.3.3-3.dmg
-
-# RedCine-X
-wget http://s3.amazonaws.com/red_3/downloads/software/rcx/REDCINE-X_PRO_Build_22.4_OSX.zip
-
-# Blender- Download with Boxen or Cask
-#wget http://download.blender.org/release/Blender2.69/blender-2.69-OSX_10.6-x86_64.zip
-#wget http://download.blender.org/release/Blender2.70/blender-2.70-OSX_10.6-x86_64.zip
-
-# DJViewer - Download with Boxen or Cask
-#wget http://sourceforge.net/projects/djv/files/djv-stable/0.9.0/djv-0.9.0_osx-x64.dmg
-
-# CUDA
-wget http://developer.download.nvidia.com/compute/cuda/5_5/rel/installers/cuda-mac-5.5.28_10.9_64.pkg
 
 # Mitsuba Renderer and Blender plugin
 wget http://www.mitsuba-renderer.org/releases/current/macos/Mitsuba%200.5.0.dmg
@@ -44,5 +13,11 @@ wget http://www.luxrender.net/release/luxrender/1.3/mac/LuxRender_1.3.1_OSXIntel
 # Aqsis Renderer
 wget http://sourceforge.net/projects/aqsis/files/aqsis-macosx/1.8.2/Aqsis-1.8.2-Darwin.dmg
 
-# Vagrant - Download with Boxen or Cask
-#https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3.dmg
+# XQuartz
+wget https://dl.bintray.com/xquartz/legacy-downloads/SL/XQuartz-2.7.8.dmg
+
+# OSXFuse
+wget http://iweb.dl.sourceforge.net/project/osxfuse/osxfuse-2.8.3/osxfuse-2.8.3.dmg
+
+# Natron
+wget http://downloads.natron.fr/Mac/releases/Natron-2.0.1.dmg
