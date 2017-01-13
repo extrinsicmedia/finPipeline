b669893c854eb6d73d49cbfb062190abd201d66d
extrinsicmedia <mileslauridsen@gmail.com>
2017-01-12 22:42:36 -0800

    Updating to the most useful installs

--- os/darwin/10.10/darwin-10.10_cask.sh
+++ os/darwin/10.10/darwin-10.10_cask.sh
@@ -1,30 +1,38 @@
 ### Cask Installs
 
+# Install Cask
 brew install caskroom/cask/brew-cask
 
-brew cask install google-chrome
-brew cask install google-drive
-brew cask install chromecast
-brew cask install firefox
-brew cask install adium
-brew cask install redcine-x-pro
-brew cask install dropbox
-brew cask install transmission
-brew cask install adobe-creative-cloud
-brew cask install backblaze
-brew cask install gimp
-brew cask install blender
-brew cask install cyberduck
-brew cask install tunnelblick
-brew cask install djv
-brew cask install handbrake
-brew cask install meshlab
-brew cask install komodo-edit
-brew cask install sequel-pro
-brew cask install silverlight
-brew cask install processing
-brew cask install skype
-brew cask install vagrant
-brew cask install virtualbox
-brew cask install vlc
-brew cask install xquartz
+# Install applications
+brew cask install adium # chat program
+brew cask install adobe-creative-cloud # adobe
+brew cask install backblaze # backup
+brew cask install blender # open source 3d
+brew cask install cuda # Cuda for NVIDIA cards
+brew cask install cyberduck # FTP program
+brew cask install djv # DJViewer open source sequence viewer
+brew cask install dnscrypt # encrypts and authenticates DNS packets
+brew cask install firefox # webrowser
+brew cask install gimp # open source image editor
+brew cask install google-chrome # webbrowser
+brew cask install google-drive # google drive
+brew cask install handbrake # open source video converter
+brew cask install java # updated java
+brew cask install libreoffice # open source office suite
+brew cask install meshlab # open source 3d conversion
+brew cask install osxfuse # used with veracrypt
+brew cask install processing # 2d image programming software
+brew cask install redcine-x-pro # Red R3D converter
+brew cask install robomongo # database viewer
+brew cask install sequel-pro # database viewer
+brew cask install skype # communication software
+brew cask install slack # communication software
+brew cask install sublime-text # text editor
+brew cask install transmission # torrent downloader
+brew cask install tunnelblick # vpn application
+brew cask install vagrant # used with virtualization software
+brew cask install veracrypt # encrypted disk software
+brew cask install virtualbox # os virtualization software
+brew cask install vlc # open source video player
+brew cask install wacom-bamboo-tablet # wacom bamboo tablet driver
+brew cask install xquartz # xquartz driver
