fb9a782f4d822f5476eedaa698befc43e86fb331
Extrinsic Media <miles@finvfx.com>
2017-01-12 21:38:24 -0800

    win10 installations to run finPipeline

---
+++ os/windows/win10/win10_installs.sh
@@ -0,0 +1,12 @@
+### Installs
+
+## Install Cygwin before continuing
+# Cygwin :: http://cygwin.com/setup-x86.exe
+# Here is a minimal list of packages for Cygwin setup
+# git python
+
+# Install Python pip
+python -m pip install -U pip setuptools
+
+# Install shyaml using pip
+pip install shyaml
