5415c338dbe7389d60a5ebc347938de4993fa4fc
Extrinsic Media <mileslauridsen@gmail.com>
2017-02-02 22:16:18 -0800

    Updating to latest config setup.

--- bash/unix_config_global.sh
+++ bash/unix_config_global.sh
@@ -15,31 +15,53 @@ then
                 export LOCAL_JOB_SERVER="$LOCAL_SERVER/PROJECTS"
                 export LOCAL_PYTHON_SERVER="$LOCAL_SYSTEMS_SERVER/python"
         fi
-        
+
         ## Set production or shared server
         PROD_SERVER_PATH="$(cat $HOME/finpipeline.yaml | shyaml get-value prod_server_path)"
-        if [[ -d $PROD_SERVER_PATH ]];
+        if [[ $PROD_SERVER_PATH != ""  ]];
+        then
+            if [[ -d $PROD_SERVER_PATH ]];
             then
                 export PROD_SERVER=$PROD_SERVER_PATH
                 export PROD_JOB_SERVER="$PROD_SERVER/PROJECTS"
                 export PROD_SYSTEMS_SERVER="$PROD_SERVER/SYSTEMS"
                 export PROD_PYTHON_SERVER="$PROD_SYSTEMS_SERVER/python"
+                unset PROD_SERVER_PATH
+                unset LOCAL_SERVER_PATH
+            fi
+        else
+            export PROD_SERVER=$LOCAL_SERVER_PATH
+            export PROD_SYSTEMS_SERVER="$LOCAL_SERVER/SYSTEMS"
+            export PROD_JOB_SERVER="$LOCAL_SERVER/PROJECTS"
+            export PROD_PYTHON_SERVER="$LOCAL_SYSTEMS_SERVER/python"
+            unset PROD_SERVER_PATH
+            unset LOCAL_SERVER_PATH
         fi
-        
+
         ## Set Dropbox path
         DROPBOX_SERVER_PATH="$(cat $HOME/finpipeline.yaml | shyaml get-value dropbox_path)"
         if [[ -d $DROPBOX_SERVER_PATH ]];
             then
                 export DROPBOX_PATH=$DROPBOX_SERVER_PATH
+                unset DROPBOX_SERVER_PATH
         fi
-        
+
+        ## Set Syncthing path
+        SYNCTHING_SERVER_PATH="$(cat $HOME/finpipeline.yaml | shyaml get-value syncthing_path)"
+        if [[ -d $SYNCTHING_SERVER_PATH ]];
+            then
+                export SYNCTHING_PATH=$SYNCTHING_SERVER_PATH
+                unset SYNCTHING_SERVER_PATH
+        fi
+
         ## Set backup server path
         BACKUP_SERVER_PATH="$(cat $HOME/finpipeline.yaml | shyaml get-value backup_server_path)"
         if [[ -d $BACKUP_SERVER_PATH ]];
             then
                 export BACKUP_SERVER=$BACKUP_SERVER_PATH
+                unset BACKUP_SERVER_PATH
         fi
-        
+
         ## Set project directory template path
         PROJECT_DIR_TEMPLATE="$(cat $HOME/finpipeline.yaml | shyaml get-value project_dir_template)"
         if [[ -d $PROJECT_DIR_TEMPLATE ]];
@@ -48,19 +70,26 @@ then
         fi
 
         ## Set reference/img path
-        REF_IMG_PATH="$(cat $HOME/finpipeline.yaml | shyaml get-value reference_img_path)"
+        REF_IMG_PATH="$(cat $HOME/finpipeline.yaml | shyaml get-value ref_img_path)"
         if [[ -d $REF_IMG_PATH ]];
             then
                 export REF_IMG_PATH=$REF_IMG_PATH
         fi
 
         ## Set reference/vid path
-        REF_VID_PATH="$(cat $HOME/finpipeline.yaml | shyaml get-value reference_vid_path)"
+        REF_VID_PATH="$(cat $HOME/finpipeline.yaml | shyaml get-value ref_vid_path)"
         if [[ -d $REF_VID_PATH ]];
             then
                 export REF_VID_PATH=$REF_VID_PATH
         fi
-        
+
+        ## Set reference/tutorial path
+        REF_TUT_PATH="$(cat $HOME/finpipeline.yaml | shyaml get-value ref_tut_path)"
+        if [[ -d $REF_TUT_PATH ]];
+        then
+            export REF_TUT_PATH=$REF_TUT_PATH
+        fi
+
         ## Set verarypt app path
         VERACRYPT_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value veracrypt_app)"
         if [[ -f $VERACRYPT_APP ]];
@@ -68,7 +97,7 @@ then
                 alias veracrypt=$VERACRYPT_APP
                 unset VERACRYPT_APP
         fi
-        
+
         ## Set vlc app path
         VLC_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value vlc_app)"
         if [[ -f $VLC_APP ]];
@@ -76,7 +105,7 @@ then
                 alias vlc=$VLC_APP
                 unset VLC_APP
         fi
-        
+
         ## Set firefox app path
         FIREFOX_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value firefox_app)"
         if [[ -f $FIREFOX_APP ]];
@@ -84,7 +113,7 @@ then
                 alias firefox=$FIREFOX_APP
                 unset FIREFOX_APP
         fi
-        
+
         ## Set djview app path
         DJVIEW_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value djview_app)"
         if [[ -f $DJVIEW_APP ]];
@@ -94,7 +123,7 @@ then
                 alias djvload='djview $(djv_ls -xi)'
                 unset DJVIEW_APP
         fi
-        
+
         ## Set djview bin path
         DJV_BIN_PATH="$(cat $HOME/finpipeline.yaml | shyaml get-value djv_bin_path)"
         if [[ -d $DJV_BIN_PATH ]];
@@ -102,7 +131,7 @@ then
                 export PATH="$PATH:$DJV_BIN_PATH"
                 unset DJV_BIN_PATH
         fi
-        
+
         ## Set bview app path
         BVIEW_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value bview_app)"
         if [[ -f $BVIEW_APP ]];
@@ -111,7 +140,7 @@ then
                 export BVIEW_PATH=$BVIEW_APP
                 unset BVIEW_APP
         fi
-        
+
         ## Set batch_bview app path
         BATCH_BVIEW_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value batch_bview_app)"
         if [[ -f $BATCH_BVIEW_APP ]];
@@ -121,6 +150,13 @@ then
                 unset BATCH_BVIEW_APP
         fi
         
+        ## Set FFMPEG path
+        FFMPEG_BIN="$(cat $HOME/finpipeline.yaml | shyaml get-value ffmpeg_bin_path)"
+        if [[ -f $FFMPEG_BIN ]];
+            then
+                export FFMPEG_BIN=$FFMPEG_BIN
+        fi
+
         ## Set rv app path
         RV_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value rv_app)"
         if [[ -f $RV_APP ]];
@@ -128,7 +164,7 @@ then
                 alias rv=$RV_APP
                 unset RV_APP
         fi
-        
+
         ## Set rvpkg app path
         RVPKG_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value rvpkg_app)"
         if [[ -f $RVPKG_APP ]];
@@ -136,7 +172,7 @@ then
                 alias rvpkg=$RVPKG_APP
                 unset RVPKG_APP
         fi
-        
+
         ## Set nuke app path
         NUKE_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value nuke_app)"
         if [[ -f $NUKE_APP ]];
@@ -144,7 +180,7 @@ then
                 alias nuke=$NUKE_APP
                 unset NUKE_APP
         fi
-        
+
         ## Set nukex app path
         NUKEX_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value nukex_app)"
         if [[ -f $NUKEX_APP ]];
@@ -152,7 +188,7 @@ then
                 alias nukex=$NUKEX_APP
                 unset NUKEX_APP
         fi
-        
+
         ## Set nukestudio app path
         NUKESTUDIO_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value nukestudio_app)"
         if [[ -f $NUKESTUDIO_APP ]];
@@ -160,7 +196,7 @@ then
                 alias nukestudio=$NUKESTUDIO_APP
                 unset NUKESTUDIO_APP
         fi
-        
+
         ## Set photoshop app path
         PHOTOSHOP_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value photoshop_app)"
         if [[ -f $PHOTOSHOP_APP ]];
@@ -168,7 +204,7 @@ then
                 alias photoshop=$PHOTOSHOP_APP
                 unset PHOTOSHOP_APP
         fi
-        
+
         ## Set aftereffects app path
         AFTEREFFECTS_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value aftereffects_app)"
         if [[ -f $AFTEREFFECTS_APP ]];
@@ -176,15 +212,15 @@ then
                 alias aftereffects=$AFTEREFFECTS_APP
                 unset AFTEREFFECTS_APP
         fi
-        
+
         ## Set maya app path
         MAYA_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value maya_app)"
         if [[ -f $MAYA_APP ]];
             then
-                alias maya=$MAYA_APP
+                alias maya="export PYTHONPATH=''; $MAYA_APP"
                 unset MAYA_APP
         fi
-        
+
         ## Set maya bin path
         MAYA_BIN_PATH="$(cat $HOME/finpipeline.yaml | shyaml get-value maya_bin_path)"
         if [[ -d $MAYA_BIN_PATH ]];
@@ -192,7 +228,28 @@ then
                 export PATH="$PATH:$MAYA_BIN_PATH"
                 unset MAYA_BIN_PATH
         fi
-        
+
+        ## Set arnold plugin path
+        ARNOLD_PLUGIN_PATH="$(cat $HOME/finpipeline.yaml | shyaml get-value arnold_plugin_path)"
+        if [[ -d $ARNOLD_PLUGIN_PATH ]];
+            then
+                export ARNOLD_PLUGIN_PATH="$ARNOLD_PLUGIN_PATH"
+        fi
+
+        ## Set mtoa templates path
+        MTOA_TEMPLATES_PATH="$(cat $HOME/finpipeline.yaml | shyaml get-value mtoa_templates_path)"
+        if [[ -d $MTOA_TEMPLATES_PATH ]];
+            then
+                export MTOA_TEMPLATES_PATH="$MTOA_TEMPLATES_PATH"
+        fi
+
+        ## Set maya template path
+        MAYA_CUSTOM_TEMPLATE_PATH="$(cat $HOME/finpipeline.yaml | shyaml get-value maya_custom_template_path)"
+        if [[ -d MAYA_CUSTOM_TEMPLATE_PATH ]];
+            then
+                export MAYA_CUSTOM_TEMPLATE_PATH="$MAYA_CUSTOM_TEMPLATE_PATH"
+        fi
+
         ## Set meshlab app path
         MESHLAB_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value meshlab_app)"
         if [[ -f $MESHLAB_APP ]];
@@ -200,18 +257,42 @@ then
                 alias meshlab=$MESHLAB_APP
                 unset MESHLAB_APP
         fi
-        
-        ## Set komodo app path
+
+        ## Set Komodo app path
         KOMODO_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value komodo_app)"
         if [[ -f $KOMODO_APP ]];
             then
                 alias komodo="\"$KOMODO_APP"\"
                 unset KOMODO_APP
         fi
+
+        ## Set Gimp app path
+        GIMP_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value gimp_app)"
+        if [[ -f $GIMP_APP ]];
+        then
+            alias gimp=$GIMP_APP
+            unset GIMP_APP
+        fi
+
+        ## Set Redcine-X app path
+        REDCINE_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value redcine_app)"
+        if [[ -f $REDCINE_APP ]];
+        then
+            alias redcine="\"$REDCINE_APP"\"
+            unset REDCINE_APP
+        fi
+
+        ## Set Redline app path
+        REDLINE_APP="$(cat $HOME/finpipeline.yaml | shyaml get-value redline_app)"
+        if [[ -f $REDLINE_APP ]];
+        then
+            alias redline="\"$REDLINE_APP"\"
+            unset REDLINE_APP
+        fi
 fi
 
 ### set server to use, if only one server is used, set $LOCAL_SERVER above
-if [[ $LOCAL_SERVER ]];
+if [[ -d $LOCAL_SERVER ]];
     then
         export SERVER="$LOCAL_SERVER"
     else
@@ -219,27 +300,22 @@ if [[ $LOCAL_SERVER ]];
 fi
 
 ### set default servers
-export LOCAL_SYSTEMS_SERVER="$LOCAL_SERVER/SYSTEMS"
-export PROD_SYSTEMS_SERVER="$PROD_SERVER/SYSTEMS"
 export SYSTEMS_SERVER="$SERVER/SYSTEMS"
-
-export LOCAL_JOB_SERVER="$LOCAL_SERVER/PROJECTS"
-export PROD_JOB_SERVER="$PROD_SERVER/PROJECTS"
 export JOB_SERVER="$SERVER/PROJECTS"
-
+export BACKUP_JOB_SERVER="$BACKUP_SERVER/PRODUCTION/PROJECTS"
 export LOCAL_DB_SERVER="$LOCAL_SYSTEMS_SERVER/database"
 export PROD_DB_SERVER="$PROD_SYSTEMS_SERVER/database"
 export DB_SERVER="$SYSTEMS_SERVER/database"
 
+### configs and naming
 export CONFIG_DIR_NAME="config"
 export CONFIG_FILE_NAME="config.sh"
 export PROD_DIR="production"
 export SEQ_NAME="sequences"
 
+### tutorials
 export LOCAL_TUTORIALS="$LOCAL_SERVER/ASSETS/tutorials"
 export PROD_TUTORIALS="$PROD_SERVER/ASSETS/tutorials"
-
-export RSYNC_EXCLUDE="$SYSTEMS_SERVER/database/txt/exclude_list.txt"
 
 ### misc stuff
 export LOCAL_APP_LIBRARY="$LOCAL_SERVER/APPS"
@@ -249,19 +325,18 @@ export APP_LIBRARY="$SERVER/APPS"
 export LOCAL_CHARTS="$LOCAL_SERVER/ASSETS/charts"
 export PROD_CHARTS="$PROD_SERVER/ASSETS/charts"
 export CHARTS="$SERVER/ASSETS/charts"
+export LUTS="$SERVER/ASSETS/luts"
 
 ### RSYNC Setups
 #alias
-export RSYNC_TIMES_DRY="--recursive --times --verbose --ignore-existing --exclude-from=$SYSTEMS_SERVER/database/txt/exclude_list.txt --dry-run"
-export RSYNC_TIMES="--recursive --times --verbose --ignore-existing --progress --exclude-from=$SYSTEMS_SERVER/database/txt/exclude_list.txt"
+export BASH_EXCLUDE_LIST="$SYSTEMS_SERVER/bash/exclude_list"
+export RSYNC_TIMES_DRY="--recursive --times --verbose --ignore-existing --exclude-from=$BASH_EXCLUDE_LIST --dry-run"
+export RSYNC_TIMES="--recursive --times --verbose --ignore-existing --progress --exclude-from=$BASH_EXCLUDE_LIST"
 alias rsynctimesdry='rsync $RSYNC_TIMES_DRY'
 alias rsynctimes='rsync $RSYNC_TIMES --log-file=$SYSTEMS_SERVER/database/logs/rsync/rsync-log-"$(date +%Y-%m-%d_%H%M%S)".log'
 
-## User Home directory backups
-alias cpuserinfo='$SYSTEMS_SERVER/bash/cpUserInfo.sh'
-
 ## Unix
-PATH="$SYSTEMS_SERVER/bin/frametools:$MAYA_LOCATION/bin:$PATH"
+PATH="$MAYA_LOCATION/bin:$PATH"
 alias makedate='mkdir $(date +%Y-%m-%d)'
 alias makedatetime='mkdir $(date +%Y-%m-%d_%H%M%S)'
 
@@ -316,3 +391,4 @@ fin () {
 
 # jobSync
 alias jobsync='$SYSTEMS_SERVER/bash/jobSync.sh'
+alias backupsync='$SYSTEMS_SERVER/bash/backupSync.sh'
